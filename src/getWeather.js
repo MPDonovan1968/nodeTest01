3077dd4a3239a865e83affd32ebd926323ce3159
MPDonovan1968 <donovanmichaelp@gmail.com>
2019-08-02 12:27:23 -0400

    Update getWeather.js

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const getWeather = (obj, callBackFx) => {
-    const urlMyHouse = 'https://api.darksky.net/forecast/e9e11af46642ebec4925a56aea644a1e/' + obj.lat + ',' + obj.long;
+    const urlMyHouse = 'https://api.darksky.net/forecast/[key goes here]/' + obj.lat + ',' + obj.long;
     request({ url: urlMyHouse, json: true }, (error, response) => {
         if (error) {
             callBackFx(error)
